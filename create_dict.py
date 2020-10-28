b0e73afc7ae2ae46821ca123fd923d147e7b60e8
IrisReijnen <55184866+IrisReijnen@users.noreply.github.com>
2020-10-28 13:46:10 +0100

    Update create_dict.py

--- create_dict.py
+++ create_dict.py
@@ -7,7 +7,7 @@
 # ---------------------------------
 # Takes MSA file with sequences and count the amount of each
 # aminoacid on a specific position.
-# Writes this to a file and visualizes this in a pie chart.
+# Visualizes this in a pie chart.
 # ---------------------------------
 # To start the program, enter this command on the CLI.
 # Replace single quotes with own input.
@@ -38,13 +38,6 @@ def main():
                     else:
                         dict_pos[seq[pos]] += 1
                 seq = ""
-    if os.path.isfile(file2):
-        os.remove(file2)
-    os.mknod(file2)
-    with open(file2, 'w') as output:
-        output.write("Position: {}\n".format(pos))
-        output.write(str(dict_pos))
-        output.write("\n\n")
     print("-" * 79)
     print("Dictionary creation complete of position {}:".format(pos + 1))
     print(dict_pos)
